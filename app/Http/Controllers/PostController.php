7886182df9513c5c3030615720822d90b5f76887
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-11-16 09:08:06 -0600

    werwer

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -108,6 +108,8 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        return $id;
+        $post=Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
